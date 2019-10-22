b23df760fbf15bd72a3d16aa269fc13cc2a59062
Tong Wu <tongwu@live.com>
2019-10-22 16:27:38 -0700

    exec self after fork to allow addr space ramdomization

--- assignment2/server.c
+++ assignment2/server.c
@@ -11,34 +11,17 @@
 int main(int argc, char const *argv[])
 {
     int server_fd, new_socket, valread;
-    struct sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
-    char buffer[1024] = {0};
-    char *hello = "Hello from server";
+    if (argc == 2) {
+        struct sockaddr_in address;
+        int opt = 1;
+        int addrlen = sizeof(address);
+        char buffer[1024] = {0};
+        char *hello = "Hello from server";
 
-    // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-    {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
+        printf("UID after fork & exec: %d\n", getuid());
+        server_fd = atoi(argv[1]);
+        printf("new exec'ed server process. file descriptor passed in using argv\nserver_fd: %d\n", server_fd);
 
-    // get the uid of "nobody" user in system
-    struct passwd *pwd = getpwnam("nobody");
-    uid_t user_id = pwd->pw_uid;
-
-    printf("UID before privilege separation: %d\n", getuid());
-    pid_t child = fork(); // split
-
-    // drop the privilege to "nobody" user to
-    // attach port and process data from the client
-    if (child == 0) {
-        
-        if (setuid(user_id) < 0) {
-            perror("set uid failed, please run with sudo");
-            exit(EXIT_FAILURE);
-        }
         // Forcefully attaching socket to the port 8080
         if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
                                                     &opt, sizeof(opt)))
@@ -70,11 +53,41 @@ int main(int argc, char const *argv[])
             exit(EXIT_FAILURE);
         }
 
-        printf("UID after privilege separation: %d\n", getuid());
         valread = read( new_socket , buffer, 1024);
         printf("%s\n",buffer );
         send(new_socket , hello , strlen(hello) , 0 );
         printf("Hello message sent\n");
+        return 0;
+    }
+
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // get the uid of "nobody" user in system
+    struct passwd *pwd = getpwnam("nobody");
+    uid_t user_id = pwd->pw_uid;
+
+    printf("UID before privilege separation: %d\n", getuid());
+
+    // drop the privilege to "nobody" user to
+    // attach port and process data from the client
+    if (fork() == 0) { // split
+        if (setuid(user_id) < 0) {
+            perror("set uid failed, please run with sudo");
+            exit(EXIT_FAILURE);
+        }
+        printf("UID after privilege separation: %d\n", getuid());
+
+        char fd_buffer [33];
+        sprintf(fd_buffer, "%d", server_fd);
+        if (execl("./server", "server", fd_buffer, NULL) < 0) {
+            perror("execl failed");
+            exit(EXIT_FAILURE);
+        }
     }
     while (wait(NULL) > 0) ; // wait for the child process to complete
 
